365e41e00011bc4b6c6b64cd21b279376cd2b639
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 16:11:38 +0530

    Update CalorieCounter.jsx

--- src/components/CalorieCounter.jsx
+++ src/components/CalorieCounter.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import "./CalorieCounter.css";
 
 function CalorieCounter() {
   return (
-    <div className="my-8">
-      <h2 className="text-2xl font-bold mb-4">Calorie Counter</h2>
+    <div className="calorie-counter">
+      <h2>Calorie Counter</h2>
+      <img src="/images/meal-plan.jpg" alt="Meal Plan" className="meal-plan-image" />
       <p>Daily calorie suggestions and tracking will be displayed here.</p>
     </div>
   );
