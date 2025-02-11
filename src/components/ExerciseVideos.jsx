4aff397672c3cc0c907807f7712d769b18c30c22
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 15:54:41 +0530

    Create ExerciseVideos.jsx

---
+++ src/components/ExerciseVideos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ExerciseVideos() {
+  return (
+    <div className="my-8">
+      <h2 className="text-2xl font-bold mb-4">Exercise Video Tutorials</h2>
+      <p>Video content for exercise tutorials goes here.</p>
+    </div>
+  );
+}
+
+export default ExerciseVideos;
