00b1a734f637b01824a60b158320bbd9d522c315
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 15:47:22 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-500 p-4 text-white text-lg">
+      FitTrack - Your Personal Fitness Dashboard
+    </nav>
+  );
+}
+
+export default Navbar;
