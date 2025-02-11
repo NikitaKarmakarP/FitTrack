d8622ac854aa7879fb9171e3baacee315ca05b5b
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 15:54:17 +0530

    Create CalorieCounter.jsx

---
+++ src/components/CalorieCounter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CalorieCounter() {
+  return (
+    <div className="my-8">
+      <h2 className="text-2xl font-bold mb-4">Calorie Counter</h2>
+      <p>Daily calorie suggestions and tracking will be displayed here.</p>
+    </div>
+  );
+}
+
+export default CalorieCounter;
