ace200ad786153460f4b9dda1d8ffd36bda6dc7d
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 16:10:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import WorkoutTracker from './components/WorkoutTracker';
-import CalorieCounter from './components/CalorieCounter';
-import ExerciseVideos from './components/ExerciseVideos';
-import CustomWorkoutPlan from './components/CustomWorkoutPlan';
-import Achievements from './components/Achievements';
+import React from "react";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import WorkoutTracker from "./components/WorkoutTracker";
+import CalorieCounter from "./components/CalorieCounter";
+import ExerciseVideos from "./components/ExerciseVideos";
+import CustomWorkoutPlan from "./components/CustomWorkoutPlan";
+import Achievements from "./components/Achievements";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="app">
       <Navbar />
-      <main className="container mx-auto p-4">
-        <WorkoutTracker />
-        <CalorieCounter />
-        <ExerciseVideos />
-        <CustomWorkoutPlan />
-        <Achievements />
-      </main>
+      <WorkoutTracker />
+      <CalorieCounter />
+      <ExerciseVideos />
+      <CustomWorkoutPlan />
+      <Achievements />
     </div>
   );
 }
