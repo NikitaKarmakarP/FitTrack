ea3e53ed465f12d4b43f3069608efd68fa868b55
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 15:55:24 +0530

    Create CustomWorkoutPlan.jsx

---
+++ src/components/CustomWorkoutPlan.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CustomWorkoutPlan() {
+  return (
+    <div className="my-8">
+      <h2 className="text-2xl font-bold mb-4">Custom Workout Plan</h2>
+      <p>Create and manage custom workout plans here.</p>
+    </div>
+  );
+}
+
+export default CustomWorkoutPlan;
