f60d4f6a79e9e80f73172529d5fe763402ef782c
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 15:52:57 +0530

    Create workoutData.js

---
+++ src/data/workoutData.js
@@ -0,0 +1,7 @@
+export const workoutData = [
+  { day: 'Mon', caloriesBurned: 300 },
+  { day: 'Tue', caloriesBurned: 450 },
+  { day: 'Wed', caloriesBurned: 500 },
+  { day: 'Thu', caloriesBurned: 350 },
+  { day: 'Fri', caloriesBurned: 600 },
+];
