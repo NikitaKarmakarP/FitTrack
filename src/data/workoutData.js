66cfbeb3be7418f5722c23cfc35133bf9f129062
Nikita Karmakar <karmakarnikita412@gmail.com>
2025-02-11 16:13:17 +0530

    Update workoutData.js

--- src/data/workoutData.js
+++ src/data/workoutData.js
@@ -1,7 +1,9 @@
-export const workoutData = [
-  { day: 'Mon', caloriesBurned: 300 },
-  { day: 'Tue', caloriesBurned: 450 },
-  { day: 'Wed', caloriesBurned: 500 },
-  { day: 'Thu', caloriesBurned: 350 },
-  { day: 'Fri', caloriesBurned: 600 },
+const workoutData = [
+  { day: "Mon", caloriesBurned: 300 },
+  { day: "Tue", caloriesBurned: 450 },
+  { day: "Wed", caloriesBurned: 400 },
+  { day: "Thu", caloriesBurned: 380 },
+  { day: "Fri", caloriesBurned: 600 },
 ];
+
+export default workoutData;
